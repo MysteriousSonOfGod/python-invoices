8e1c7a0fec0419f8db567670d7f02ab7833cadeb
Jakub Kustra <kuba.kustra@gmail.com>
2019-12-25 22:58:10 +0100

    Made home window a central widget of the main window

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5 import uic
 
 app = QApplication(sys.argv)
-main_window = uic.loadUi("/home/kuba/PycharmProjects/python-invoices/src/resources/pyqt/main_window.ui")
+main_window = uic.loadUi("/home/kuba/PycharmProjects/python-invoices/resources/pyqt/main_window.ui")
+home_window = uic.loadUi("/home/kuba/PycharmProjects/python-invoices/resources/pyqt/home_window.ui")
+main_window.setCentralWidget(home_window)
 main_window.show()
 sys.exit(app.exec())
