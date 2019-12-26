81b6b63a528302066ceb47a0059188d25508517c
Jakub Kustra <kuba.kustra@gmail.com>
2019-12-26 22:18:20 +0100

    Rewrote the main script

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication
-from PyQt5 import uic
 
-app = QApplication(sys.argv)
-main_window = uic.loadUi("/home/kuba/PycharmProjects/python-invoices/resources/pyqt/main_window.ui")
-home_window = uic.loadUi("/home/kuba/PycharmProjects/python-invoices/resources/pyqt/home_window.ui")
-main_window.setCentralWidget(home_window)
-main_window.show()
-sys.exit(app.exec())
+from resources.windows.main_window import MainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    sys.exit(app.exec())
