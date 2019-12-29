47966b5482f2cce383eb93eeb3ffc87dc5f1a410
Jakub Kustra <kuba.kustra@gmail.com>
2019-12-29 23:22:47 +0100

    Finished laying out TableWidget in home window

--- src/windows/home_window.py
+++ src/windows/home_window.py
@@ -1,11 +1,34 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget
+import sys
 
+from PyQt5 import uic, QtWidgets
+from PyQt5.QtWidgets import QWidget, QMessageBox
+from sqlalchemy import exc
+
+from database import data
 from pyqt.reference_classes.home_window import Ui_HomeWindow
-
 
 class HomeWindow(QWidget, Ui_HomeWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        uic.loadUi("pyqt/home_window.ui")
+
+        # Setting the columns' widths
+        header = self.tableWidget.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        column_widths = (75, 50, 95, 50, 100, 90, 90, 105)
+        for i, w in enumerate(column_widths):
+            self.tableWidget.setColumnWidth(i + 1, w)
+            header.setSectionResizeMode(i + 1, QtWidgets.QHeaderView.Fixed)
+
+    def display_data(self):
+        session = data.Session()
+        try:
+            pass
+        except exc.IntegrityError as errmsg:
+            print(errmsg)
+            session.rollback()
+            session.close()
+            msg = QMessageBox(icon=QMessageBox.Critical, text="Błąd krytyczny bazy danych",
+                              title="Błąd krytyczny", buttons=QMessageBox.Ok)
+            msg.setDetailedText(errmsg)
+            msg.buttonClicked.connect(sys.exit)
