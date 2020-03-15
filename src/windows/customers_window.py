ba3376caa2891d4b135ce75ead8c0de7ac44a21b
Jakub Kustra <kuba.kustra@gmail.com>
2020-03-15 17:42:27 +0100

    Implemented proper resizing of the QTableView's columns

--- src/windows/customers_window.py
+++ src/windows/customers_window.py
@@ -1,25 +1,41 @@
-from PyQt5 import uic
-from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5 import uic, QtWidgets
+from PyQt5.QtGui import QStandardItemModel, QStandardItem, QFont
 from PyQt5.QtWidgets import QWidget
 
+from database import data
 from pyqt.reference_classes.customers_window import Ui_CustomersWindow
 
 
 class CustomersWindow(QWidget, Ui_CustomersWindow):
-    def __init__(self, session, customers):
+    def __init__(self, session):
         super().__init__()
-        self.customers = customers
         self.session = session
         self.setupUi(self)
         uic.loadUi("pyqt/customers_window.ui")
         self.sti = QStandardItemModel()
         self.fill_table()
+        self.customersTableView.setModel(self.sti)
+
+        # https://stackoverflow.com/questions/26681578/qtableview-column-width
+        self.build_table()
+
+    def build_table(self):
+        # Setting the columns' widths
+        self.sti.setHorizontalHeaderLabels([
+            "Alias", "Imię", "Nazwisko", "PESEL/NIP", "Nazwa\nfirmy", "Adres", "Kod\npocztowy", "Miasto", "Płatność"
+        ])
+        header = self.customersTableView.horizontalHeader()
+        # https://doc.qt.io/qt-5/qfont.html#Weight-enum
+        header.setFont(QFont("Sans Serif", pointSize=15, weight=75, italic=False))
+        header.setFixedHeight(40)
+        column_widths = (180, 100, 200, 180, 150, 200, 100, 150, 90)
+        for i, w in enumerate(column_widths):
+            self.customersTableView.setColumnWidth(i, w)
+            header.setSectionResizeMode(i, QtWidgets.QHeaderView.Fixed)
 
     def fill_table(self):
-        self.sti.setHorizontalHeaderLabels([
-            "Alias", "Imię", "Nazwisko", "PESEL/NIP", "Nazwa firmy", "Adres", "Kod pocztowy", "Miasto", "Płatność"
-        ])
-        for cust in self.customers:
+        self.sti.clear()
+        for cust in self.session.query(data.Customer):
             self.add_new_row(cust)
 
     def add_new_row(self, customer):
